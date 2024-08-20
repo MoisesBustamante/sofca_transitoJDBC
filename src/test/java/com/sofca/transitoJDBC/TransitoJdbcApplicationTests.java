0cc8e797a52366e580aa395dad67a8d15ed02ae1
Jose Barrios <josealfredobarriossalado@gmail.com>
2024-08-20 11:14:00 -0500

    first commit

---
+++ src/test/java/com/sofca/transitoJDBC/TransitoJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofca.transitoJDBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitoJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
